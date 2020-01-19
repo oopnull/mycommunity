14bdc6c44f5e8f579b3c3bbd90d69d776943fb86
mcy <1056096898@qq,com>
2020-01-19 11:37:49 +0800

    init repo

---
+++ src/test/java/my/pro/acommunity/AcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package my.pro.acommunity;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
